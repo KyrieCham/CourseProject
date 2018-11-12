b834fad0fd19c72f45582baca3f9d57ca63b6282
Xinyu Xu <xinyu17@stanford.edu>
2018-11-11 17:09:33 -0800

    delete data with no reviews Please enter the commit message for your changes. Lines starting

--- preprocess.py
+++ preprocess.py
@@ -22,23 +22,42 @@ def load_csv(csv_path):
     # Load features and labels
     x_cols = [i for i in range(len(headers)) if headers[i] == 'reviews.text']
     l_cols = [i for i in range(len(headers)) if headers[i] == 'reviews.rating']
+    print(l_cols)
 
     # inputs = np.loadtxt(csv_path, delimiter=',', skiprows=1, usecols=x_cols)
     # labels = np.loadtxt(csv_path, delimiter=',', skiprows=1, usecols=l_cols)
-
+    j = 0
     with open('review_full.txt', 'w') as f:
-        with open(csv_path, 'r') as csvFile:
-            reader = csv.reader(csvFile)
-            for row in reader:
-                f.write(row[x_cols[0]])
-                f.write('\n')
-
-    with open('rate_full.txt', 'w') as f:
-        with open(csv_path, 'r') as csvFile:
-            reader = csv.reader(csvFile)
-            for row in reader:
-                f.write(row[l_cols[0]])
-                f.write('\n')
+        with open('rate_full.txt', 'w') as g:
+            with open(csv_path, 'r') as csvFile:
+                reader = csv.reader(csvFile)
+                for row in reader:
+                    # if j == 0:
+                    #     continue
+                    # filter the rate
+                    if row[l_cols[0]] != '':
+                        j += 1
+                        f.write(row[x_cols[0]])
+                        g.write(row[l_cols[0]])
+                        f.write('\n')
+                        g.write('\n')
+                    # if j==100:
+                    #     break
+                print('review lines', j)
+    # with open('rate_full.txt', 'w') as f:
+    #     with open(csv_path, 'r') as csvFile:
+    #         reader = csv.reader(csvFile)
+    #         for row in reader:
+    #             # if j == 0:
+    #             #     continue
+    #             if row == '' or None:
+    #                 print('there is no rate')
+    #             j += 1
+    #             f.write(row[l_cols[0]])
+    #             f.write('\n')
+    #             # if j==100:
+    #             #     break
+    #         print('rate lines', j)
     # csvFile.close()
     # with open(csv_path, 'r', newline='') as csv_fh:
     #     j = 0
